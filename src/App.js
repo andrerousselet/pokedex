dcaafb1574c0d541bc3b2d462d45fa25f08bff3c
Andre Rousselet <andre098@hotmail.com>
2022-11-21 09:02:16 -0300

    feat(app.js): fetch pokemons data and add button to load more pokemons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { requestData } from './services/request'
+import Sidebar from './components/Sidebar';
+import Pokemons from './components/Pokemons';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [next, setNext] = useState('');
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const data = await requestData('https://pokeapi.co/api/v2/pokemon');
+      setNext(data.next);
+      setPokemons(data.results)
+    }
+    fetchPokemons();
+  }, []);
+
+  const fetchMorePokemons = async () => {
+    const data = await requestData(next);
+    setNext(data.next);
+    setPokemons([...pokemons, ...data.results]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main className='main-content'>
+        <Sidebar />
+        <section className='pokemons'>
+          <Pokemons pokemons={pokemons} />
+          <section className='loader-section'>
+            <button
+              className='load-btn'
+              type='button'
+              onClick={fetchMorePokemons}
+            >
+              Load more pokemons...
+            </button>
+          </section>
+        </section>
+      </main>
+    </>
   );
 }
 
